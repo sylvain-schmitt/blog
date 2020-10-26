ffa584feee061ac153be5622f270cd9b653d9921
sylvain-schmitt <69637905+sylvain-schmitt@users.noreply.github.com>
2020-10-26 12:52:14 +0100

    Update updateArticle.php
    ajout d'une value id lors de la modification d'article

--- updateArticle.php
+++ updateArticle.php
@@ -112,7 +112,7 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
                         <select class="form-control" name="category">
                             <option type="text" value="<?= $title['category_id']?>">choisir une catégorie</option>
                             <?php foreach($categorys as $category ): ?>
-                            <option ><?= $category['id'] ?> : <?= $category['category_name'] ?></option>
+                            <option value="<?= $category['id'] ?>"><?= $category['category_name'] ?></option>
                             <?php endforeach; ?>
                         </select>
                     </div>       
